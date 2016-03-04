c450dc85f6409e51c4c8c14533e27ef49cfdcb71
mazarag2 <mazarag2@asu.edu>
2016-03-03 18:57:08 -0700

    Fixed problems according to code review and eliminate unnecessary data fields

--- src/net/sf/memoranda/ui/LOCTable.java
+++ src/net/sf/memoranda/ui/LOCTable.java
@@ -14,25 +14,12 @@ public class LOCTable extends JFrame  {
 
 	private JPanel contentPane;
 	private JTable table;
-	private final int COLUMN = 2;
-	private final Object[] COLUMNNAMES	= {"Source File","LOC"};
-	
-	
-	public LOCTable(int row) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
-		table = new JTable(row,COLUMN);
-		contentPane.add(table, BorderLayout.CENTER);
-		
-		JLabel lblImportedLocTable = new JLabel("Imported LOC Table");
-		contentPane.add(lblImportedLocTable, BorderLayout.NORTH);
-	}
-	public LOCTable(Object[][] data,Object[] ColumnNmaes){
+
+	/*
+	 * Constructor for LOCTable takes in a 2D array of data and array columnNames
+	 */
+	public LOCTable(Object[][] data,Object[] ColumnNames)
+	{
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -40,12 +27,9 @@ public class LOCTable extends JFrame  {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
-		table = new JTable(data,COLUMNNAMES);
+		table = new JTable(data,ColumnNames);
 		contentPane.add(table, BorderLayout.CENTER);
 		contentPane.add(table.getTableHeader(),BorderLayout.PAGE_START);
-		//JLabel lblImportedLocTable = new JLabel("Imported LOC Table");
-		//contentPane.add(lblImportedLocTable, BorderLayout.NORTH);
 		this.setVisible(true);
 		this.setTitle("Import LOC");
 		JScrollPane scroll = new JScrollPane(table);
