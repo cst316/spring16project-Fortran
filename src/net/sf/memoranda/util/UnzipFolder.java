978f4e5a29a190b00c4500a71ca9590d6965d1d3
mazarag2 <mazarag2@asu.edu>
2016-03-24 18:01:34 -0700

    Conducted Black Box Unit test with UnzipFolder.java

--- src/net/sf/memoranda/util/UnzipFolder.java
+++ src/net/sf/memoranda/util/UnzipFolder.java
@@ -3,12 +3,12 @@ package net.sf.memoranda.util;
 import java.io.*;
 import java.util.zip.*;
 
-import org.apache.commons.io.FilenameUtils;
+//import org.apache.commons.io.FilenameUtils;
 
 import java.util.Enumeration;
 import java.io.IOException;
 
-public class UnZipFolder{
+public class UnzipFolder{
 	//constants & field names
 	private final int MAXLIMIT = 2048;
 	/** .memoranda location:: drive:\Users\<user>\.memoranda*/
@@ -18,13 +18,20 @@ public class UnZipFolder{
 	private String file;
 	/** fileName only <p>Ex: ZipFullOfJava*/
 	private String zipFileName;
-
+	private boolean isZipNested = false;
+	public boolean isNestedZipThrown() {
+		return isZipNested;
+	}
+	public void setNestedZipThrown(boolean nestedZipThrown) {
+		isZipNested = nestedZipThrown;
+	}
 	//construct(s)
 	/**Constructs UnzipFolder object with predefined and/or passed value(s).
 	 * @param zipFile File to unzip. Assumes file has '.zip' extension.
 	 */
-	public UnZipFolder(File zipFile){
-		extract (zipFile);
+	public UnzipFolder(File zipFile){
+		file = zipFile.getName();
+		isZipNested = false;
 	}
 	//getters & setters
 	/**  @return FOLDERDEST folder destination
@@ -46,35 +53,43 @@ public class UnZipFolder{
 	}
 
 	//method(s)
-	/**  @param zipFile File to unzip. Assumes file has '.zip' extension.
+	/**  @param zipFile File to unzip. 
 	 */
-	public void extract (File zipFile){
+	public boolean extract (File zipFile){
 		//obtain zipfile's files
-		file = zipFile.getName();
 		zipFileName = file.substring(0,file.length() - 4);
 		new File(zipFileName).mkdir();
 		//unzip file and extract java files
 		 try {
-			 System.out.println(zipFile.getPath());
+			 //System.out.println(zipFile.getPath());
 			 BufferedOutputStream dest = null;
 	         BufferedInputStream is = null;
 	         ZipEntry entry;
 	         ZipFile zipfile = new ZipFile(zipFile);
 	         Enumeration e = zipfile.entries();
-	         while(e.hasMoreElements()) {
-
+	         File destFile;
+	         while(e.hasMoreElements() && isZipNested == false) {
+	        	 
 	            entry = (ZipEntry) e.nextElement();
 
 	            String currentEntry = entry.getName();
-	            System.out.print(currentEntry);
-	          	File destFile = new File(FOLDERDEST + zipFileName, currentEntry);
+	            //System.out.println(currentEntry);
+	            //For Unit Testing Purposes
+	            String path = "test/";//GOMaDIIIT ITS IN HERERERE MODIFY FOR boi
+	          
+	          	//File destFile = new File(FOLDERDEST + zipFileName, currentEntry);
+	            destFile = new File(path + zipFileName, currentEntry);
+	             //destFile = new File(path,currentEntry);
 	            File destinationParent = destFile.getParentFile();
-	            destinationParent.mkdir();
-
+	            
+	            if(destinationParent != null){
+	            	destinationParent.mkdirs();
+	            }
+	           
 	            if(!entry.isDirectory()) {
 
-	            	System.out.println("Extracting: " +entry);
-	            	//Quy Ly's Code 43-51 prints filename
+	            	//System.out.println("Extracting: " +entry);
+	            	//Quy Ly's Code 
 	            	if (currentEntry.contains(".java")) {
 						//If current Entry is
 						//If lastindexof is ".java", does not give the full name
@@ -82,9 +97,8 @@ public class UnZipFolder{
 						int index = currentEntry.lastIndexOf("/");
 						String fN = currentEntry.substring(index + 1);
 						//for testing purposes
-						System.out.println(fN);
+						//System.out.println(fN);
 					}
-
 	            	is = new BufferedInputStream
 	            			(zipfile.getInputStream(entry));
 	            	int count;
@@ -100,13 +114,25 @@ public class UnZipFolder{
 	            	dest.close();
 	            	is.close();
 	            }
-
+	            if(currentEntry.endsWith(".zip")){
+	            	//set to true exit loop and abort import
+	            	isZipNested = true;
+	            		
+	            }
+	             
 	         }
          	zipfile.close();
-		 } catch (IOException e) {
+		 } 
+		catch (IOException e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage() + "Error in reading/extracting zip.");
 			// TODO: handle exception
 		}
-	 }//extract method
-}//UnzipFolder class
+		 finally {
+			
+			 return isZipNested; 
+			 
+		}
+		 
+	 }
+}
