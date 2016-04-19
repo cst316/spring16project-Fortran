27e8026c88809a8c448cb006d10d9edbf50c97a4
mazarag2 <mazarag2@asu.edu>
2016-04-18 19:01:13 -0700

    Refactored Unit Tests

---
+++ src/TestClasses/StopWatchUnitTesting.java
@@ -0,0 +1,44 @@
+package TestClasses;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import net.sf.memoranda.ui.StopWatch;
+
+public class StopWatchUnitTesting {
+
+	/*
+	 * This method case tests for the name of the task 
+	 * created by the default constructors of TimerTask
+	 */
+	@Test
+	public void test1() {
+		StopWatch task1 = new StopWatch();
+		String temp1 = "Task";
+		assertTrue(!(temp1 == task1.name));
+	}
+	
+	/*
+	 * This method case tests for the name of the task 
+	 * created by the one string param constructors of TimerTask
+	 */
+	@Test
+	public void test2() {
+		StopWatch task2 = new StopWatch("MyFirstTask");
+		String temp2 = "MyFirstTask";
+		assertEquals(temp2, task2.name);
+	}
+	/*
+	 * This method case tests for the name of the task
+	 * to see how TimerTask behaves when we provide a name with spaces
+	 */
+	@Test
+	public void test3(){
+		StopWatch task3 = new StopWatch("My First Task");
+		String temp3 = "My First Task";
+		assertTrue(temp3 == task3.name);
+		
+	}
+}
